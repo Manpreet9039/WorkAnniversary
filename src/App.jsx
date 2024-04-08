ce31cbe920ff1c606d59715ad746fbe0938cac9b
Manpreet9039 <manpreetkaur622495@gmail.com>
2024-04-08 12:36:51 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+
+import './App.css'
+//import BirthdayMail from './BirthdayMail'
+
+//import Birthday1 from './Birthday1'
+import WorkAnniversaryMail2 from './WorkAnniversaryMail2'
+//import Work from './Work'
+//import WorkAnniversary1 from './WorkAnniversary1'
+
+
+
+function App() {
+  
+  return (
+    <>
+      {/* <WorkAnniversary1 /> */}
+      
+      {/* <Birthday1 /> */}
+
+      <WorkAnniversaryMail2/>
+      
+      {/* <BirthdayMail /> */}
+      
+    </>
+  )
+}
+
+export default App
